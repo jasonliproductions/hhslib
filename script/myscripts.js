17b2458900c7512ecb7931ce6378f8dcbeca6689
jasonliproductions <115770961+jasonliproductions@users.noreply.github.com>
2022-11-22 18:03:06 +0800

    Updates to API search
    -API data successfully fetched
    -Rudimentary search function has been added into search box: All results will be shown inside the the terminal.
    -Added training functions to the drop down list.

--- script/myscripts.js
+++ script/myscripts.js
@@ -1,8 +1,19 @@
 function debugFunction(){
-	alert("Debug successful");
+    var selectedCenters = [];
+    var centerNo = 0;
+    var keyword = document.getElementById("keyword").value;
+
+    for(i=0; i<7; i++){
+        centerNo++;
+        if (document.getElementById("center" + centerNo).checked == true){
+            selectedCenters.push(document.getElementById("center" + centerNo).value);
+        }
+    }
+
+    alert(keyword);
 }
 
-function openCity(evt, cityName) {
+function libTab(evt, tabTitle) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -12,6 +23,57 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabTitle).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function searchFunction(){
+    var centerNo = 0;
+    var keyword = document.getElementById("keyword").value;
+    var training_function = document.getElementById("training_function").value;
+    var selectedCenters = [];
+    
+    for(i=0; i<7; i++){
+        centerNo++;
+        if (document.getElementById("center" + centerNo).checked == true){
+            selectedCenters.push(document.getElementById("center" + centerNo).value);
+        }
+    }
+    
+    const data = {
+        keyword: keyword,
+        training_function: { 
+            prc: [training_function], 
+            sccc: [] 
+        },
+        cent_code: selectedCenters
+    };
+    
+    fetch('https://prctoylib.heephong.org:10443/sku/search', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+            appendData(data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function appendData(data){
+    var mainDiv = document.getElementById("search_results");
+
+    for (i=0; i < data.length; i++){
+        var div = document.createElement("div");
+        div.innerHTML= 
+        data[i].title;
+        mainDiv.appendChild(div);
+    }
 }
